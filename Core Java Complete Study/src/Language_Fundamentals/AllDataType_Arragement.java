df044939ffe327016cc7e873025c9352899fdb91
Shubham Gajanan Borkar <shubhamgborkar99@gmail.com>
2024-09-15 20:44:41 +0530

    "code pushed successfully"

---
+++ Core Java Complete Study/src/Language_Fundamentals/AllDataType_Arragement.java
@@ -0,0 +1,17 @@
+package Language_Fundamentals;
+
+import java.util.*;
+
+public class AllDataType_Arragement {
+
+	public static void main() 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the data to check the range of data type at runtime :");
+		
+		String str =sc.next();
+		
+		
+	}
+}
+	
