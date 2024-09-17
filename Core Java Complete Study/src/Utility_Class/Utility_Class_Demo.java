3346b76a05ea6ea3844ea73ff795e37146267a18
Shubham Gajanan Borkar <shubhamgborkar99@gmail.com>
2024-09-17 23:26:19 +0530

    "practice pushed successfully"

---
+++ Core Java Complete Study/src/Utility_Class/Utility_Class_Demo.java
@@ -0,0 +1,20 @@
+package Utility_Class;
+
+public class Utility_Class_Demo {
+
+	private Utility_Class_Demo()
+	{
+		throw new UnsupportedOperationException("Utility class ");
+	}
+	
+	public static void show()
+	{
+		System.out.println("i am show method :");
+	}
+	
+	public static void main(String x[])
+	{
+		
+		
+	}
+}
