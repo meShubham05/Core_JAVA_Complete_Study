c3880157171d7f2efcd6b3901fd296b8c90699ab
Shubham Gajanan Borkar <shubhamgborkar99@gmail.com>
2024-09-23 22:35:15 +0530

    "practice pushed successfully"

---
+++ Core Java Complete Study/src/Abstract_Classes/Abstract_Class_Demo.java
@@ -0,0 +1,27 @@
+package Abstract_Classes;
+
+ abstract class Parent {
+
+	public  abstract  void show();
+	
+}
+
+class Child extends Parent
+ {
+	 
+	  public void show()
+	  {
+		  System.out.println("I am Child Show Method :");
+	  }
+ }
+
+public class  Abstract_Class_Demo
+{
+	public static void main(String[] args) 
+	{
+		
+		Child  c = new Child();
+		c.show();
+		
+	}
+}
